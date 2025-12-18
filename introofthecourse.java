09a05f21395a069104e014741abe240c201fece4
sivalikannan <sivalimkannan@gmail.com>
2025-12-18 19:58:33 +0530

    tnsif day one commit

---
+++ introofthecourse.java
@@ -0,0 +1,14 @@
+package DayOne;
+
+public class introofthecourse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Day one of tnsif java full stack course");
+		
+		//camel case: eg, day One//
+		//pascal case: eg, Day One//
+
+	}
+
+}
